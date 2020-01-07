9db415767d4341e56cca7a3cd558042f06b68516
Adam Dodman <adam@adam-ant.co.uk>
2020-01-07 22:15:18 +0000

    Use less stupid timestamp calculation, cleanup a little

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 from flask import Flask, request, abort
-from datetime import datetime
 import logging
 import json
 import configparser
@@ -12,15 +11,11 @@ log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
 def doNotify(success, build):
-    if success:
-        status = "SUCCESS"
-    else:
-        status = "FAILURE"
 
-    #WHY doesnt timedelta have a formatting option.
-    datediff = (datetime.fromtimestamp(build["build"]["updated"]) - datetime.fromtimestamp(build["build"]["started"]))
-    minutes,seconds=divmod(datediff.seconds, 60)
-    datestr = ("{:02}m{:02}s".format(minutes, seconds))
+    status = ("SUCCESS" if success else "FAILURE")
+
+    minutes,seconds=divmod(int(build["build"]["updated"]) - int(build["build"]["started"]), 60)
+    datestr = "{:02}m{:02}s".format(minutes, seconds)
 
     drone_link = "{}/{}/{}".format(build["system"]["link"], build["repo"]["slug"], build["build"]["number"])
 
@@ -30,17 +25,18 @@ def doNotify(success, build):
         commit_firstline = build["build"]["message"]
         commit_rest = ""
 
-    print(commit_firstline)
-
+    # TODO: Add PR functionality
     notifymsg="<b>{repo} [{branch}]</b> #{number}: <b>{status}</b> in {time}\n<a href='{drone_link}'>{drone_link}</a>\n<a href='{git_link}'>#{commit:7.7}</a> ({committer}): <i>{commit_firstline}</i>\n{commit_rest}".format(
                     repo=build["repo"]["slug"], branch=build["build"]["target"], number=build["build"]["number"], status=status, time=datestr, drone_link=drone_link,
                     git_link=build["build"]["link"], commit=build["build"]["after"], committer=build["build"]["author_login"], commit_firstline=commit_firstline, commit_rest=commit_rest)
+
     postdata = {
             "parse_mode": "html",
             "disable_web_page_preview": "true",
             "chat_id": tchat,
             "text": notifymsg
     }
+
     try:
         r = requests.post("https://api.telegram.org/bot{}/sendmessage".format(ttoken), json=postdata)
         print(r.text)
@@ -58,7 +54,6 @@ def webhook():
                 for step in stage['steps']:
                     status = step['status']
                     if status == 'failure':
-                        # Do Notify
                         doNotify(False, json)
                         return 'epicfail', 200
                     if status != 'success':
