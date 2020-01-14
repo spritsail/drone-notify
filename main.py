47b6c7b9542c1b894aaf4bcf817c37df2880ef3a
Adam Dodman <github@adam-ant.co.uk>
2020-01-14 01:12:30 +0000

    Revert "Escape input with html.escape, fixes HTTP 400"
    This reverts commit 45bfdd02ccafd225aa19134a76692833f4fc92af.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 from bottle import run, post, request
-from html import escape
 import datetime
-import json
 import logging
+import json
 import os
 import requests
 
@@ -23,7 +22,7 @@ def doNotify(success, build):
     isPR = ""
     if (build["build"]["event"] == "pull_request"):
         # This isn't pretty, but it works.
-        isPR = "#{PR_Num} → ".format(PR_Num=escape(build["build"]["ref"].split("/")[2]))
+        isPR = "#{PR_Num} → ".format(PR_Num=build["build"]["ref"].split("/")[2])
 
     multi_stage = ""
 
@@ -37,12 +36,8 @@ def doNotify(success, build):
 
     if (len(build["build"]["stages"]) > 1):
         for stage in build["build"]["stages"]:
-            multi_stage += "• {stage_name}     <b>{stage_state}</b> in {time} {emoji}\n".format(
-                    stage_name=escape(stage["name"]),
-                    stage_state=escape(stage["status"]),
-                    time=calcTime(stage["started"], stage["stopped"]),
-                    emoji=emojiDict.get(stage["status"], "❔")
-            )
+            stageline = "• {stage_name}     <b>{stage_state}</b> in {time} {emoji}\n".format(stage_name=stage["name"], stage_state=stage["status"], time=calcTime(stage["started"], stage["stopped"]), emoji=emojiDict.get(stage["status"], "❔"))
+            multi_stage += stageline
         multi_stage += "\n"
 
     drone_link = "{}/{}/{}".format(build["system"]["link"], build["repo"]["slug"], build["build"]["number"])
@@ -54,24 +49,9 @@ def doNotify(success, build):
         commit_firstline = build["build"]["message"]
         commit_rest = ""
 
-    notifymsg="<b>{repo} [{PR}{branch}]</b> #{number}: <b>{status}</b> in {time}\n" + \
-              "<a href='{drone_link}'>{drone_link}</a>\n" + \
-              "{multi_stage}<a href='{git_link}'>#{commit:7.7}</a> ({committer}): <i>{commit_firstline}</i>" + \
-              "\n{commit_rest}".format(
-                    PR=isPR,
-                    branch=escape(build["build"]["target"]),
-                    commit=escape(build["build"]["after"]),
-                    commit_firstline=escape(commit_firstline),
-                    commit_rest=escape(commit_rest),
-                    committer=escape(build["build"]["author_login"]),
-                    drone_link=escape(drone_link),
-                    git_link=escape(build["build"]["link"]),
-                    multi_stage=multi_stage,
-                    number=build["build"]["number"],
-                    repo=escape(build["repo"]["slug"]),
-                    status=escape(status),
-                    time=calcTime(build["build"]["started"], build["build"]["finished"]),
-    )
+    notifymsg="<b>{repo} [{PR}{branch}]</b> #{number}: <b>{status}</b> in {time}\n<a href='{drone_link}'>{drone_link}</a>\n{multi_stage}<a href='{git_link}'>#{commit:7.7}</a> ({committer}): <i>{commit_firstline}</i>\n{commit_rest}".format(
+                    repo=build["repo"]["slug"], PR=isPR, branch=build["build"]["target"], number=build["build"]["number"], status=status, time=calcTime(build["build"]["started"], build["build"]["finished"]),
+                    drone_link=drone_link, multi_stage=multi_stage, git_link=build["build"]["link"], commit=build["build"]["after"], committer=build["build"]["author_login"], commit_firstline=commit_firstline, commit_rest=commit_rest)
 
     postdata = {
             "parse_mode": "html",
